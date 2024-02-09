9a6b267a35982eca18d0d06ba82eedbdece60f12
santymm95 <santymm95@gmail.com>
2024-02-09 16:28:53 -0500

    DarkModeReact-v1.01

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class", // Note: Removed the quotes around darkMode
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
